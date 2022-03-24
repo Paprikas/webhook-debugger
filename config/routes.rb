39fd3bcd471e34f90200309266d88a9805f18203
Maxim Zelenkin <nudepatch@gmail.com>
2022-03-24 06:33:20 +0500

    Add create_webhook_request url to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :webhook_requests, only: :index
   delete "webhook_requests", to: "webhook_requests#delete_all", as: :delete_all_requests
-  match "create_webhook_request", to: "webhook_requests#create", via: :all
+  match "create_webhook_request", to: "webhook_requests#create", via: :all, as: :create_webhook_request
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
