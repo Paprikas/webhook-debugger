9e8380c1c43544227658cf78a42f2aaa375a9ea1
Maxim Zelenkin <nudepatch@gmail.com>
2022-03-25 01:53:19 +0500

    Add awesome_print for better raw_post inspection

--- app/models/webhook_request.rb
+++ app/models/webhook_request.rb
@@ -2,4 +2,10 @@ class WebhookRequest < ApplicationRecord
   enum :request_method, ActionDispatch::Request::HTTP_METHODS
 
   after_create_commit -> { broadcast_prepend_to :webhook_requests }
+
+  def parsed_raw_post
+    JSON.parse(raw_post)
+  rescue
+    raw_post
+  end
 end
