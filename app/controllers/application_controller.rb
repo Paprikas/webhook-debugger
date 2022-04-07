43d527f002ff9ea2ea48feead16c8f833c2a89fa
Maxim Zelenkin <nudepatch@gmail.com>
2022-04-07 21:33:50 +0500

    standardrb --fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate!
     http_basic_authenticate_or_request_with name: ENV.fetch("HTTP_BASIC_AUTH_NAME", ""),
-                                            password: ENV.fetch("HTTP_BASIC_AUTH_PASSWORD", ""),
-                                            message: "Your credentials to access located in ENV vars. Check out the Readme for more details."
+      password: ENV.fetch("HTTP_BASIC_AUTH_PASSWORD", ""),
+      message: "Your credentials to access located in ENV vars. Check out the Readme for more details."
   end
 end
