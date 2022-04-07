d19284cb64d68150d4b6b0d27e1a8a3fb548dbdc
Maxim Zelenkin <nudepatch@gmail.com>
2022-04-07 22:31:35 +0500

    #1 Use tailwindcss-stimulus-components for flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def tailwind_classes_for(flash_type)
+    {
+      notice: "bg-green-600 text-white",
+      alert: "bg-red-600 text-white"
+    }.stringify_keys[flash_type.to_s] || "bg-gray-600 text-white"
+  end
 end
